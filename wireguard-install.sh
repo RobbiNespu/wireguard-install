f9b6bd28908e78003126dad81a3cebb365873e16
Restyled.io <commits@restyled.io>
2021-11-26 01:25:27 +0000

    Restyled by shellharden

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -288,11 +288,11 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 		if [[ "$os" == "ubuntu" ]]; then
 			# Ubuntu
 			apt-get update
-			apt-get install -y wireguard qrencode $firewall
+			apt-get install -y wireguard qrencode "$firewall"
 		elif [[ "$os" == "debian" && "$os_version" -ge 11 ]]; then
 			# Debian 11 or higher
 			apt-get update
-			apt-get install -y wireguard qrencode $firewall
+			apt-get install -y wireguard qrencode "$firewall"
 		elif [[ "$os" == "debian" && "$os_version" -eq 10 ]]; then
 			# Debian 10
 			if ! grep -qs '^deb .* buster-backports main' /etc/apt/sources.list /etc/apt/sources.list.d/*.list; then
@@ -310,21 +310,21 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 			# headers were still downloadable and to provide suitable headers for future
 			# kernel updates.
 			apt-get install -y linux-headers-"$architecture"
-			apt-get install -y wireguard qrencode $firewall
+			apt-get install -y wireguard qrencode "$firewall"
 		elif [[ "$os" == "centos" && "$os_version" -eq 8 ]]; then
 			# CentOS 8
 			dnf install -y epel-release elrepo-release
-			dnf install -y kmod-wireguard wireguard-tools qrencode $firewall
+			dnf install -y kmod-wireguard wireguard-tools qrencode "$firewall"
 			mkdir -p /etc/wireguard/
 		elif [[ "$os" == "centos" && "$os_version" -eq 7 ]]; then
 			# CentOS 7
 			yum install -y epel-release https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 			yum install -y yum-plugin-elrepo
-			yum install -y kmod-wireguard wireguard-tools qrencode $firewall
+			yum install -y kmod-wireguard wireguard-tools qrencode "$firewall"
 			mkdir -p /etc/wireguard/
 		elif [[ "$os" == "fedora" ]]; then
 			# Fedora
-			dnf install -y wireguard-tools qrencode $firewall
+			dnf install -y wireguard-tools qrencode "$firewall"
 			mkdir -p /etc/wireguard/
 		fi
 	# Else, we are inside a container and BoringTun needs to be used
@@ -333,12 +333,12 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 		if [[ "$os" == "ubuntu" ]]; then
 			# Ubuntu
 			apt-get update
-			apt-get install -y qrencode ca-certificates $cron $firewall
+			apt-get install -y qrencode ca-certificates "$cron" "$firewall"
 			apt-get install -y wireguard-tools --no-install-recommends
 		elif [[ "$os" == "debian" && "$os_version" -ge 11 ]]; then
 			# Debian 11 or higher
 			apt-get update
-			apt-get install -y qrencode ca-certificates $cron $firewall
+			apt-get install -y qrencode ca-certificates "$cron" "$firewall"
 			apt-get install -y wireguard-tools --no-install-recommends
 		elif [[ "$os" == "debian" && "$os_version" -eq 10 ]]; then
 			# Debian 10
@@ -346,21 +346,21 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 				echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list
 			fi
 			apt-get update
-			apt-get install -y qrencode ca-certificates $cron $firewall
+			apt-get install -y qrencode ca-certificates "$cron" "$firewall"
 			apt-get install -y wireguard-tools --no-install-recommends
 		elif [[ "$os" == "centos" && "$os_version" -eq 8 ]]; then
 			# CentOS 8
 			dnf install -y epel-release
-			dnf install -y wireguard-tools qrencode ca-certificates tar $cron $firewall
+			dnf install -y wireguard-tools qrencode ca-certificates tar "$cron" "$firewall"
 			mkdir -p /etc/wireguard/
 		elif [[ "$os" == "centos" && "$os_version" -eq 7 ]]; then
 			# CentOS 7
 			yum install -y epel-release
-			yum install -y wireguard-tools qrencode ca-certificates tar $cron $firewall
+			yum install -y wireguard-tools qrencode ca-certificates tar "$cron" "$firewall"
 			mkdir -p /etc/wireguard/
 		elif [[ "$os" == "fedora" ]]; then
 			# Fedora
-			dnf install -y wireguard-tools qrencode ca-certificates tar $cron $firewall
+			dnf install -y wireguard-tools qrencode ca-certificates tar "$cron" "$firewall"
 			mkdir -p /etc/wireguard/
 		fi
 		# Grab the BoringTun binary using wget or curl and extract into the right place.
